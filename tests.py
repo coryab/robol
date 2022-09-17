dfbfaae6f580ce2543f279f0ed855ab5d0d63a9d
Cory Balaton <cory@balaton.dev>
2022-09-17 23:13:30 +0200

    Changed test1 so that it works.

--- tests.py
+++ tests.py
@@ -1,24 +1,36 @@
 # Import block
 from typing import List
+from robol import *
 
 class TestCode:
     def __init__(self):
         pass
 
-    def test1():
-        grid: Grid = Grid(NumberExp(64), NumberExp(64))
-        start: Start = Start(NumberExp(23), NumberExp(30))
+    def test1(self):
+        robot: Robot = Robot()
+        grid: Grid = Grid(NumberExp(64, robot), NumberExp(64, robot), robot)
+        start: Start = Start(NumberExp(23, robot), NumberExp(30, robot), robot)
         statements: List = []
-        statements.append(Turn(Direction.clockwise))
-        statements.append(Turn(Direction.clockwise))
-        statements.append(Step(NumberExp(15)))
-        statements.append(Turn(Direction.counterclockwise))
-        statements.append(Step(NumberExp(15)))
-        statements.append(Turn(Direction.counterclockwise))
-        statements.append(Step(ArithmeticExp(BinaryOp.PLUS, NumberExp(2), NumberExp(3))))
-        statements.append(Turn(Direction.counterclockwise))
-        statements.append(Step(ArithmeticExp(BinaryOp.PLUS, NumberExp(17), NumberExp(20))))
+        statements.append(Turn(Direction.CLOCKWISE, robot))
+        statements.append(Turn(Direction.CLOCKWISE, robot))
+        statements.append(Step(NumberExp(15, robot), robot))
+        statements.append(Turn(Direction.COUNTERCLOCKWISE, robot))
+        statements.append(Step(NumberExp(15, robot), robot))
+        statements.append(Turn(Direction.COUNTERCLOCKWISE, robot))
+        statements.append(Step(ArithmeticExp(BinaryOp.PLUS,\
+                NumberExp(2, robot), NumberExp(3, robot), robot), robot))
+        statements.append(Turn(Direction.COUNTERCLOCKWISE, robot))
+        statements.append(Step(ArithmeticExp(BinaryOp.PLUS,\
+                NumberExp(17, robot), NumberExp(20, robot), robot), robot))
+        statements.append(Stop(robot))
 
-        program: Program = Program(grid, )
+        robot.start = start
+        robot.statements = statements
+        program: Program = Program(grid, robot)
 
         program.interpret()
+
+
+if __name__ == "__main__":
+    tests = TestCode()
+    tests.test1()
